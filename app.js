5e61569662a4b5873c74236da923250c4f896d43
George Higbie <gehigbie@gmail.com>
2017-06-23 20:18:19 +0000

    Change file structure and paths

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const express = require('express'),
 app.set('port', process.env.PORT);
 app.set('ip', process.env.IP);
 
+app.use(express.static(__dirname, "/public/shells/"));
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(app.get('port'), app.get('ip'), () => {
     var port = app.get('port');
     console.log(`Server is listening on port ${port}`); 
